24e4cc7252cf920fc1c6ba8f38d3f31a4f339321
oO-Blink-Oo <alvarezian23@gmail.com>
2019-09-15 05:31:13 -0700

    finished chapter 7

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
 var DETAIL_TITLE_SELECTOR = "[data-image-role=\"title\"]";
+var DETAIL_FRAME_SELECTOR = "[data-image-role=\"frame\"]";
 var THUMBNAIL_LINK_SELECTOR = "[data-image-role=\"trigger\"]";
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var TINY_EFFECT_CLASS = "is-tiny";
+var ESC_KEY = 27; // this is the ASCII value
 
 function setDetails(imageUrl, titleText) {
   "use strict";
@@ -34,10 +38,10 @@ function setDetailsFromThumb(thumbnail) {
 
 function addThumbClickHandler(thumb) {
   "use strict";
-
   thumb.addEventListener("click", function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 
@@ -48,10 +52,40 @@ function getThumbnailsArray() {
   return thumbnailsArray;
 }
 
+function hideDetails() {
+  "use strict";
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  "use strict";
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+
+
+}
+
+function addKeyPressHandler() {
+  "use strict";
+  document.body.addEventListener("keyup", function (event){
+    event.preventDefault();
+    /* eslint-disable-next-line no-console */
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
 function initializeEvents() {
   "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
